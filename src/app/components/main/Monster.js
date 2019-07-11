35e7003e2e2ebe946003e84d1611cd93d909adea
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-07-11 18:25:58 +0900

    apply attack style/range

--- src/app/components/main/Monster.js
+++ src/app/components/main/Monster.js
@@ -12,19 +12,37 @@ import React, { Component } from "react";
 
 export default class Monster extends Component {
   render() {
+    const aRange = {
+      en: ["Melee", "Ranged"],
+      kr: ["단거리", "원거리"]
+    };
+    const aType = {
+      en: ["Aggrresive", "Non-Aggressive"],
+      kr: ["선공", "후공"]
+    };
+    const mType = {
+      en: ["Normal", "Semi-Named", "Named"],
+      kr: ["일반몹", "세미네임드", "네임드"]
+    };
     return (
       <div className={"Monster " + (this.props.disable ? "disable" : "")}>
         <div className="Monster-name">{this.props.info[this.props.lang]}</div>
         <div className="Monster-image">
-          <img src={"img/" + this.props.info.en + ".png"} alt="img" />
+          <img src={"img/" + this.props.info["en"] + ".png"} alt="img" />
         </div>
-        <div className="Monster-level">{this.props.info.level}</div>
-        <div className="Monster-range">근거리</div>
-        <div className="Monster-style">선공</div>
-        <div className="Monster-respawn">30 초</div>
-        <div className="Monster-exp">60</div>
+        <div className="Monster-level">{this.props.info["level"]}</div>
+        <div className="Monster-range">
+          {aRange[this.props.lang][this.props.info["attack range"]]}
+        </div>
+        <div className="Monster-style">
+          {aType[this.props.lang][this.props.info["attack style"]]}
+        </div>
+        <div className="Monster-respawn">{this.props.info["respawn time"]}</div>
+        <div className="Monster-exp">{this.props.info["exp"]}</div>
         <div className="Monster-item">아이템</div>
-        <div className="Monster-type">일반몹</div>
+        <div className="Monster-type">
+          {mType[this.props.lang][this.props.info["type"]]}
+        </div>
         {/* <div className="Monster-lvl">{console.log(this.props.info)}</div> */}
       </div>
     );
